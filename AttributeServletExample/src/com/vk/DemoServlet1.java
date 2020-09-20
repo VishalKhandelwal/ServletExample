10e24e79cac5c14565b812ea830ac07f6e1b6585
Vishal Khandelwal <vishalshekhar9316@gmail.com>
2020-09-20 12:11:44 +0530

    Attribute Example

---
+++ AttributeServletExample/src/com/vk/DemoServlet1.java
@@ -0,0 +1,35 @@
+package com.vk;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DemoServlet1
+ */
+@WebServlet("/DemoServlet1")
+public class DemoServlet1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		ServletContext context = getServletContext();
+		context.setAttribute("Company", "IBM");
+		out.println("Welcome to first servlet");
+		out.println("<a href ='DemoServlet2'>visit</a>");
+		out.close();
+	}
+
+}
