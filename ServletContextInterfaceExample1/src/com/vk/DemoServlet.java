0e8a1538fd09402bb57626de3516597739e7fdc4
Vishal Khandelwal <vishalshekhar9316@gmail.com>
2020-09-20 12:07:09 +0530

    servlet context interface example

---
+++ ServletContextInterfaceExample1/src/com/vk/DemoServlet.java
@@ -0,0 +1,38 @@
+package com.vk;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DemoServlet
+ */
+@WebServlet("/DemoServlet")
+public class DemoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		//creating ServletContext object 
+		ServletContext context = getServletContext();
+		
+		//Getting the value of the initialization parameter and printing it  
+		String driverName = context.getInitParameter("dName");
+		out.println("driver name is="+driverName);
+		out.close();
+	}
+
+}
