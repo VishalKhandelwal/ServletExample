11b7f4cd1ca99ec8726d11e493f1b6abcfedc629
Vishal Khandelwal <vishalshekhar9316@gmail.com>
2020-09-20 15:41:15 +0530

    URL Rewriting Example

---
+++ UrlRewritingExample/src/com/vk/Servlet1.java
@@ -0,0 +1,39 @@
+package com.vk;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet1
+ */
+@WebServlet("/Servlet1")
+public class Servlet1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");  
+        PrintWriter out = response.getWriter();  
+          
+        String n=request.getParameter("userName");  
+        out.print("Welcome "+n);  
+  
+        //appending the username in the query string  
+        out.print("<a href='Servlet2?uname="+n+"'>visit</a>");  
+                  
+        out.close();  
+  
+               
+	}
+
+}
